da2c9d3d1783c2291cd335b38de900d1f2a0cdd2
jcheron <myaddressmail@gmail.com>
2020-03-19 09:18:36 +0100

    api & store configuration

--- config/environment.js
+++ config/environment.js
@@ -46,6 +46,9 @@ module.exports = function(environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
   }
+  ENV.contentSecurityPolicy = {
+    'connect-src': "'self' http://127.0.0.1:8080"
+  };
 
   return ENV;
 };
